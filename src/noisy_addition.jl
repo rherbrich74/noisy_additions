f7300a25e15bb5b92b3d1bf9f4feef74332d7d26
Ralf Herbrich <rherbrich@gmail.com>
2023-01-03 22:50:24 +0000

    Added the 8-bit adder

--- src/noisy_addition.jl
+++ src/noisy_addition.jl
@@ -117,6 +117,43 @@ function full_adder(ha::Dict{Tuple{UInt8,UInt8},Dict{Tuple{UInt8,UInt8},Float64}
 end
 
 """
+    four_bit_adder(ha, fa, A, B)
+
+Returns a dictionary that computes the probability distribution of a probabilistic 4bit-adder for the two inputs `A` and `B` (based on probabilistic `ha` (half-adder), `fa` (full-adder) gates).
+"""
+function four_bit_adder(ha::Dict{Tuple{UInt8,UInt8},Dict{Tuple{UInt8,UInt8},Float64}},
+                        fa::Dict{Tuple{UInt8,UInt8,UInt8},Dict{Tuple{UInt8,UInt8},Float64}},
+                        A, B)::Dict{UInt8,Float64}
+    a0 = A & 1; A >>= 1
+    a1 = A & 1; A >>= 1
+    a2 = A & 1; A >>= 1
+    a3 = A & 1
+    b0 = B & 1; B >>= 1
+    b1 = B & 1; B >>= 1
+    b2 = B & 1; B >>= 1
+    b3 = B & 1
+    p_dict = Dict{UInt8,Float64}()
+    for out = 0:31
+        S = out
+        s0 = out & 1; out >>= 1
+        s1 = out & 1; out >>= 1
+        s2 = out & 1; out >>= 1
+        s3 = out & 1; out >>= 1
+        s4 = out & 1
+        p = 0
+        for marginal = 0:7
+            # extract the invidiual bits
+            c0 = marginal & 1; marginal >>= 1
+            c1 = marginal & 1; marginal >>= 1
+            c2 = marginal & 1
+            p += ha[(a0,b0)][(s0,c0)] * fa[(a1,b1,c0)][(s1,c1)] * fa[(a2,b2,c1)][(s2,c2)] * fa[(a3,b3,c2)][(s3,s4)]
+        end
+        p_dict[S] = p
+    end
+    return(p_dict)
+end
+
+"""
     four_bit_adder(ha, fa)
 
 Returns a dictionary that computes the probability distribution of a probabilistic 4bit-adder (based on probabilistic `ha` (half-adder), `fa` (full-adder) gates).
@@ -127,35 +164,64 @@ function four_bit_adder(ha::Dict{Tuple{UInt8,UInt8},Dict{Tuple{UInt8,UInt8},Floa
     for inp = 0:255
         A = inp & 15
         B = (inp >> 4) & 15
-        a0 = inp & 1; inp >>= 1
-        a1 = inp & 1; inp >>= 1
-        a2 = inp & 1; inp >>= 1
-        a3 = inp & 1; inp >>= 1
-        b0 = inp & 1; inp >>= 1
-        b1 = inp & 1; inp >>= 1
-        b2 = inp & 1; inp >>= 1
-        b3 = inp & 1
-        p_dict = Dict{UInt8,Float64}()
-        for out = 0:31
-            S = out
-            s0 = out & 1; out >>= 1
-            s1 = out & 1; out >>= 1
-            s2 = out & 1; out >>= 1
-            s3 = out & 1; out >>= 1
-            s4 = out & 1
-            p = 0
-            for marginal = 0:7
-                # extract the invidiual bits
-                c0 = marginal & 1; marginal >>= 1
-                c1 = marginal & 1; marginal >>= 1
-                c2 = marginal & 1
-                p += ha[(a0,b0)][(s0,c0)] * fa[(a1,b1,c0)][(s1,c1)] * fa[(a2,b2,c1)][(s2,c2)] * fa[(a3,b3,c2)][(s3,s4)]
-            end
-            p_dict[S] = p
-        end
-        D[(A,B)] = p_dict
+        D[(A,B)] = four_bit_adder(ha, fa, A, B)
     end
     return(D)
+end
+
+"""
+    eight_bit_adder(ha, fa, A, B)
+
+Returns a dictionary that computes the probability distribution of a probabilistic 8bit-adder for the two inputs `A` and `B` (based on probabilistic `ha` (half-adder), `fa` (full-adder) gates).
+"""
+function eight_bit_adder(ha::Dict{Tuple{UInt8,UInt8},Dict{Tuple{UInt8,UInt8},Float64}},
+                         fa::Vector{Dict{Tuple{UInt8,UInt8,UInt8},Dict{Tuple{UInt8,UInt8},Float64}}},
+                         A, B)::Dict{UInt16,Float64}
+    a0 = A & 1; A >>= 1
+    a1 = A & 1; A >>= 1
+    a2 = A & 1; A >>= 1
+    a3 = A & 1; A >>= 1
+    a4 = A & 1; A >>= 1
+    a5 = A & 1; A >>= 1
+    a6 = A & 1; A >>= 1
+    a7 = A & 1
+    b0 = B & 1; B >>= 1
+    b1 = B & 1; B >>= 1
+    b2 = B & 1; B >>= 1
+    b3 = B & 1; B >>= 1
+    b4 = B & 1; B >>= 1
+    b5 = B & 1; B >>= 1
+    b6 = B & 1; B >>= 1
+    b7 = B & 1
+    p_dict = Dict{UInt16,Float64}()
+    for out = 0:511
+        S = out
+        s0 = out & 1; out >>= 1
+        s1 = out & 1; out >>= 1
+        s2 = out & 1; out >>= 1
+        s3 = out & 1; out >>= 1
+        s4 = out & 1; out >>= 1
+        s5 = out & 1; out >>= 1
+        s6 = out & 1; out >>= 1
+        s7 = out & 1; out >>= 1
+        s8 = out & 1
+        p = 0
+        for marginal = 0:127
+            # extract the invidiual bits
+            c0 = marginal & 1; marginal >>= 1
+            c1 = marginal & 1; marginal >>= 1
+            c2 = marginal & 1; marginal >>= 1
+            c3 = marginal & 1; marginal >>= 1
+            c4 = marginal & 1; marginal >>= 1
+            c5 = marginal & 1; marginal >>= 1
+            c6 = marginal & 1
+            p += ha[(a0,b0)][(s0,c0)] * fa[1][(a1,b1,c0)][(s1,c1)] * fa[2][(a2,b2,c1)][(s2,c2)] * 
+                 fa[3][(a3,b3,c2)][(s3,c3)] * fa[4][(a4,b4,c3)][(s4,c4)] * fa[5][(a5,b5,c4)][(s5,c5)] * 
+                 fa[6][(a6,b6,c5)][(s6,c6)] * fa[7][(a7,b7,c6)][(s7,s8)]
+        end
+        p_dict[S] = p
+    end
+    return(p_dict)
 end
 
 """
@@ -266,16 +332,17 @@ end
 # print_distribution()
 
 gr()
-x = 6
-y = 4
+x = 254
+y = 1
 α = 0
 anim = @animate for β ∈ range(0,0.5, length=100)
-    ha = half_adder(pnand(α,β),pnor(α,β),pnot(α,β))
-    fa = full_adder(ha,pnor(α,β),pnot(α,β))
-    fb = four_bit_adder(ha, fa)
-    bar(map(kv->kv[2], sort([(k,v) for (k,v) in fb[(x,y)]], by=first)), legend=false)
+    β_range = map(i -> β/i, 1:8)
+    ha = map(i -> half_adder(pnand(α,β_range[i]),pnor(α,β_range[i]),pnot(α,β_range[i])),1:8) 
+    fa = map(i -> full_adder(ha[i],pnor(α,β_range[i]),pnot(α,β_range[i])),1:8)
+    fb = eight_bit_adder(ha[1], fa[2:8], x, y)
+    bar(map(kv->kv[2], sort([(k,v) for (k,v) in fb], by=first)), legend=false)
     title!(sprintf1("Probability Distribution over $x + $y for β=%5.2f", β), fontsize=12)
     xlabel!("sum of $x + $y")
 end
-gif(anim, "anim_fps15.gif", fps = 15)
+gif(anim, "anim_254_1_fps15.gif", fps = 15)
 
