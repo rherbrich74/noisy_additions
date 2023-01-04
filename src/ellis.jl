799226ba6cb9bdbf106e8f294933de155ca833aa
Ralf Herbrich <rherbrich@gmail.com>
2023-01-04 16:38:24 +0000

    Adjusted the ELLIS unconference plots

--- src/ellis.jl
+++ src/ellis.jl
@@ -30,7 +30,7 @@ function plot_one_valued_logic(f,α=0)
 end
 
 """
-    plot_two_valued_logic(f, inpu=(0,0), α=0, plot_title=true)
+    plot_two_valued_logic(f, inp=(0,0), α=0, plot_title=true)
 
 Plots the probability distribution of a two-valued logic function `f` for a fixed input `inp` over all values of β and the value of `α`
 """
@@ -53,49 +53,99 @@ function plot_two_valued_logic(f, inp=(0,0), α=0, plot_title=true)
     return (plt)
 end
 
+"""
+    plot_two_valued_error(f_map, α=0)
+
+Plots the error distribution for a two-valued logic function `f` over all values of β and the value of `α`
+"""
+function plot_two_valued_error(f; α=0, legend_pos=:bottomright)
+    # compute the true output
+    f_map = f(0,0)
+    true_output = Dict{keytype(f_map),Tuple{UInt8,UInt8}}()
+    for (k,v) in f_map
+        for (k2,v2) in v
+            if (v2 == 1) 
+                true_output[k] = k2
+                break
+            end
+        end
+    end 
+
+    # now plot the error rates for all the possible inputs
+    colors = [:red, :green, :blue, :black, :cyan, :brown, :gray, :yellow]
+    β_range = range(0, 0.5, length=100)
+    plt = plot(xlims = (0,0.5), ylims = (0,1), grid=true)
+    i = 1
+    for key in collect(keys(f(α,0)))
+        plot_key = key
+        plot!(plt, β_range, map(β -> 1 - f(α,β)[key][true_output[key]], β_range), 
+              legend=legend_pos,
+              ylims=(0,1),
+              linecolor=colors[((i-1) % length(colors)) + 1], 
+              linewidth=2.5, label="P(error|inp=$plot_key)", fontsize=10)
+        i += 1
+    end
+    xlabel!(plt, "β", fontsize=12)
+    ylabel!(plt, sprintf1("P(error|α=%5.2f)", α), fontsize=12)
+    title!(plt, sprintf1("Error Rate for α=%5.2f", α), fontsize=12)
+    return (plt)
+end
 
 
 # # plot basic logic functions
-# gr()
-# plot_one_valued_logic(pnand, 0.1) |> display 
-# readline()
+gr()
+anim = @animate for α ∈ range(0,0.5, length=100)
+    plot_one_valued_logic(pnand, α) 
+end
+gif(anim, "anim_basic_correct_fps15.gif", fps = 15)
 
 # plot half adder logic functions
 gr()
-p1 = plot_two_valued_logic((α,β) -> half_adder(pnand(α,β),pnor(α,β),pnot(α,β)), (0,0))
-p2 = plot_two_valued_logic((α,β) -> half_adder(pnand(α,β),pnor(α,β),pnot(α,β)), (1,0))
-p3 = plot_two_valued_logic((α,β) -> half_adder(pnand(α,β),pnor(α,β),pnot(α,β)), (0,1))
-p4 = plot_two_valued_logic((α,β) -> half_adder(pnand(α,β),pnor(α,β),pnot(α,β)), (1,1))
-plot(p1, p2, p3, p4, layout=(2,2), size=(1200,1000)) |> display
-readline()
+anim = @animate for α ∈ range(0,0.5, length=100)
+    plot_two_valued_error((α,β) ->
+        half_adder(pnand(α,β),pnor(α,β),pnot(α,β)), α=α)
+    
+end
+gif(anim, "anim_ha_error_fps15.gif", fps = 15)
 
-# # plot half adder logic functions
-# gr()
-# anim = @animate for α ∈ range(0,0.5, length=100)
-#     p1 = plot_two_valued_logic((α,β) -> half_adder(pnand(α,β),pnor(α,β),pnot(α,β)), (0,0), α, false)
-#     p2 = plot_two_valued_logic((α,β) -> half_adder(pnand(α,β),pnor(α,β),pnot(α,β)), (1,0), α, false)
-#     p3 = plot_two_valued_logic((α,β) -> half_adder(pnand(α,β),pnor(α,β),pnot(α,β)), (0,1), α, false)
-#     p4 = plot_two_valued_logic((α,β) -> half_adder(pnand(α,β),pnor(α,β),pnot(α,β)), (1,1), α, false)
-#     plot(p1, p2, p3, p4, layout=(2,2), size=(1200,1000))
-# end
-# gif(anim, "anim_fps15.gif", fps = 15)
+# plot full adder logic functions
+gr()
+anim = @animate for α ∈ range(0,0.5, length=100)
+    plot_two_valued_error((α,β) ->
+        full_adder(half_adder(pnand(α,β),pnor(α,β),pnot(α,β)),pnor(α,β),pnot(α,β)), α=α)
+    
+end
+gif(anim, "anim_fa_error_fps15.gif", fps = 15)
 
-# # prints and checks the probability distributions on screen
-# println(check_distribution(full_adder(half_adder(pnand(0.1,0.2),pnor(0.1,0.2),pnot(0.1,0.2)),pnor(0.1,0.2),pnot(0.1,0.2))))
-# print_distribution()
+# plot the error distribution without correction
+gr()
+x = 100
+y = 88
+α = 0
+anim = @animate for β ∈ range(0,0.5, length=100)
+    β_range = map(i -> β, 1:8)
+    ha = map(i -> half_adder(pnand(α,β_range[i]),pnor(α,β_range[i]),pnot(α,β_range[i])),1:8) 
+    fa = map(i -> full_adder(ha[i],pnor(α,β_range[i]),pnot(α,β_range[i])),1:8)
+    fb = eight_bit_adder(ha[1], fa[2:8], x, y)
+    bar(map(kv->kv[2], sort([(k,v) for (k,v) in fb], by=first)), legend=false)
+    title!(sprintf1("Probability Distribution over $x + $y for β=%5.2f", β), fontsize=12)
+    xlabel!("sum of $x + $y")
+end
+gif(anim, "anim_100_88_no_decay_fps15.gif", fps = 15)
 
-# gr()
-# x = 254
-# y = 1
-# α = 0
-# anim = @animate for β ∈ range(0,0.5, length=100)
-#     β_range = map(i -> β/i, 1:8)
-#     ha = map(i -> half_adder(pnand(α,β_range[i]),pnor(α,β_range[i]),pnot(α,β_range[i])),1:8) 
-#     fa = map(i -> full_adder(ha[i],pnor(α,β_range[i]),pnot(α,β_range[i])),1:8)
-#     fb = eight_bit_adder(ha[1], fa[2:8], x, y)
-#     bar(map(kv->kv[2], sort([(k,v) for (k,v) in fb], by=first)), legend=false)
-#     title!(sprintf1("Probability Distribution over $x + $y for β=%5.2f", β), fontsize=12)
-#     xlabel!("sum of $x + $y")
-# end
-# gif(anim, "anim_254_1_fps15.gif", fps = 15)
 
+# plot the error distribution without correction
+gr()
+x = 100
+y = 88
+α = 0
+anim = @animate for β ∈ range(0,0.5, length=100)
+    β_range = map(i -> β/i, 1:8)
+    ha = map(i -> half_adder(pnand(α,β_range[i]),pnor(α,β_range[i]),pnot(α,β_range[i])),1:8) 
+    fa = map(i -> full_adder(ha[i],pnor(α,β_range[i]),pnot(α,β_range[i])),1:8)
+    fb = eight_bit_adder(ha[1], fa[2:8], x, y)
+    bar(map(kv->kv[2], sort([(k,v) for (k,v) in fb], by=first)), legend=false)
+    title!(sprintf1("Probability Distribution over $x + $y for β=%5.2f", β), fontsize=12)
+    xlabel!("sum of $x + $y")
+end
+gif(anim, "anim_100_88_linear_decay_fps15.gif", fps = 15)
